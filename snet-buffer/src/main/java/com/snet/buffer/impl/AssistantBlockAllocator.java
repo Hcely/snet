d4baec73ea63b9e74b98eeb7254963314b917e10
hcely <yepeijie1900@126.com>
2019-11-17 21:26:05 +0800

    update

--- snet-buffer/src/main/java/com/snet/buffer/impl/AssistantBlockAllocator.java
+++ snet-buffer/src/main/java/com/snet/buffer/impl/AssistantBlockAllocator.java
@@ -8,13 +8,30 @@ public class AssistantBlockAllocator implements SNetResourceBlockAllocator {
 	public static final int MIN_CAPACITY = 1 << 6;
 	public static final int BLOCK_CAPACITY = 1 << 12;
 
+	@SuppressWarnings("unchecked")
 	private static BlockList<FixedResourceBlock>[] newBlockLists() {
-		return null;
+		BlockList<FixedResourceBlock> list0_0 = new BlockList<>(0, 0);
+		BlockList<FixedResourceBlock> list1_30 = new BlockList<>(list0_0, 30);
+		BlockList<FixedResourceBlock> list31_70 = new BlockList<>(list1_30, 70);
+		BlockList<FixedResourceBlock> list71_99 = new BlockList<>(list31_70, 99);
+		BlockList<FixedResourceBlock> list100 = new BlockList<>(list71_99, 100);
+		return new BlockList[]{list31_70, list1_30, list71_99, list100};
 	}
 
-	protected MainBlockAllocator parentAllocator;
-	protected FixedResourceBlock[] blockList;
-	protected FixedQueue<SNetResourceBlock>[] caches;
+	protected final MainBlockAllocator parentAllocator;
+	protected final BlockList<FixedResourceBlock>[][] blockLists;
+	protected final FixedQueue<SNetResourceBlock>[] caches;
+
+	@SuppressWarnings("unchecked")
+	public AssistantBlockAllocator(MainBlockAllocator parentAllocator) {
+		this.parentAllocator = parentAllocator;
+		this.blockLists = new BlockList[6][];
+		this.caches = new FixedQueue[6];
+		for (int i = 0; i < 6; ++i) {
+			blockLists[i] = newBlockLists();
+			caches[i] = new FixedQueue<>(64);
+		}
+	}
 
 	@Override
 	public SNetResourceBlock allocate(int capacity) {
